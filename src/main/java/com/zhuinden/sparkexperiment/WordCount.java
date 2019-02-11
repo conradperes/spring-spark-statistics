1862fd6576d765c383a5150261f2a979be5f924a
conradperes <conrad.marques.peres@gmail.com>
2019-02-11 14:59:04 -0200

    updating to total of 404 http codes group by date

--- src/main/java/com/zhuinden/sparkexperiment/WordCount.java
+++ src/main/java/com/zhuinden/sparkexperiment/WordCount.java
@@ -79,52 +79,73 @@ public class WordCount {
         JavaRDD<Row> rowRDD = lines.toJavaRDD().map(RowFactory::create);
         List<StructField> fields = Arrays.asList(
                 DataTypes.createStructField("line", DataTypes.StringType, true));
-        StructType schema = DataTypes.createStructType(fields);
-        SQLContext sqlContext = new SQLContext(sc);
-        Dataset<Row> df = sqlContext.createDataFrame(rowRDD, schema);
-        return df.filter(col("line").endsWith("404 -"));
-    }
-
-    public List<Count> count404GroupedByDay() {
-        int partitionCount = 100;
-        SparkContext sc = sparkSession.sparkContext();
-        RDD<String> lines = sc.textFile(HADOOP_URI + FILE_NAME, 3 * sc.defaultParallelism()).cache();
-        long count = lines.count();
-        //List<Word>words  = (List<Word>) lines.toJavaRDD().map(Word::new);
-        //List<StructField> fields = Arrays.asList(
-        //        DataTypes.createStructField("line",  DataTypes.StringType, true));
-        //StructType schema = DataTypes.createStructType(fields);
-        SQLContext sqlContext = new SQLContext(sc);
-        StructType schema = DataTypes.createStructType(new StructField[] {
+        StructType schema =
+         DataTypes.createStructType(new StructField[] {
                 DataTypes.createStructField("url",  DataTypes.StringType, true),
                 DataTypes.createStructField("data", DataTypes.TimestampType, true),
                 DataTypes.createStructField("chamada", DataTypes.StringType, true),
                 DataTypes.createStructField("httpcode", DataTypes.IntegerType, true),
                 DataTypes.createStructField("qtde", DataTypes.IntegerType, true)
         });
-        Dataset<Row> df = sqlContext.createDataFrame(lines, Word.class);
-        Dataset errors = df.filter(col("httpcode").endsWith("404 -"));
-        // Counts all the errors
-        errors.count();
-        // Counts errors mentioning MySQL
-        errors.filter(col("httpcode").like("%MySQL%")).count();
-        // Fetches the MySQL errors as an array of strings
-        errors.filter(col("httpcode").like("%MySQL%")).collect();
-
-        RelationalGroupedDataset groupedDataset = errors.groupBy(col("data"));
-        System.out.println("####################" +
-                "Total de 404 agrupados por data:"+groupedDataset.count()
-        +"####################");
-        groupedDataset.count().show();
-        List<Row> rows = groupedDataset.count().collectAsList();//JavaConversions.asScalaBuffer(words)).count();
-        return rows.stream().map(new Function<Row, Count>() {
-            @Override
-            public Count apply(Row row) {
-                return new Count(row.getString(0), row.getLong(1));
-            }
-        }).collect(toList());
-
+        SQLContext sqlContext = new SQLContext(sc);
+        Dataset<Row> df = sqlContext.createDataFrame(rowRDD, schema);
+        return df.filter(col("line").endsWith("404 -"));
     }
+
+//    public List<Count> count404GroupedByDay() {
+//        int partitionCount = 100;
+//        SparkContext sc = sparkSession.sparkContext();
+//        RDD<String> lines = sc.textFile(HADOOP_URI + FILE_NAME, 3 * sc.defaultParallelism()).cache();
+//        JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+//
+//            private static final long serialVersionUID = 1L;
+//
+//            // Here the first line will be passed, for example, hello you.
+//            // The Iterable<String>(hello, you) is returned.
+//            @Override
+//            public Iterator<String> call(String t) throws Exception {
+//                return Arrays.asList(t.endsWith("404 -");
+//            }
+//
+//        });
+//
+//        long count = lines.count();
+//
+//        //List<Word>words  = (List<Word>) lines.toJavaRDD().map(Word::new);
+//        //List<StructField> fields = Arrays.asList(
+//        //        DataTypes.createStructField("line",  DataTypes.StringType, true));
+//        //StructType schema = DataTypes.createStructType(fields);
+//        SQLContext sqlContext = new SQLContext(sc);
+//        StructType schema = DataTypes.createStructType(new StructField[] {
+//                DataTypes.createStructField("url",  DataTypes.StringType, true),
+//                DataTypes.createStructField("data", DataTypes.TimestampType, true),
+//                DataTypes.createStructField("chamada", DataTypes.StringType, true),
+//                DataTypes.createStructField("httpcode", DataTypes.IntegerType, true),
+//                DataTypes.createStructField("qtde", DataTypes.IntegerType, true)
+//        });
+//        Dataset<Row> df = sqlContext.createDataFrame(lines, schema);
+//        Dataset errors = df.filter(col("httpcode").endsWith("404 -"));
+//        // Counts all the errors
+//        errors.count();
+//        // Counts errors mentioning MySQL
+//        errors.filter(col("httpcode").like("%MySQL%")).count();
+//        // Fetches the MySQL errors as an array of strings
+//        errors.filter(col("httpcode").like("%MySQL%")).collect();
+//
+//        RelationalGroupedDataset groupedDataset = errors.groupBy(col("data"));
+//        System.out.println("####################" +
+//                "Total de 404 agrupados por data:"+groupedDataset.count()
+//        +"####################");
+//        groupedDataset.count().show();
+//        List<Row> rows = groupedDataset.count().collectAsList();//JavaConversions.asScalaBuffer(words)).count();
+//        return rows.stream().map(new Function<Row, Count>() {
+//            @Override
+//            public Count apply(Row row) {
+//                return new Count(row.getString(0), row.getLong(1));
+//            }
+//        }).collect(toList());
+//
+//    }
 
 
 
@@ -134,7 +155,7 @@ public class WordCount {
         Dataset errors = getDataset(HADOOP_URI , FILE_NAME);
         //List<Word>words = errors.collectAsList().stream().map(Word::new).collect(toList());
 
-        RelationalGroupedDataset groupedDataset = errors.groupBy(col("word"));
+        RelationalGroupedDataset groupedDataset = errors.groupBy(col("data"));
         groupedDataset.count().show();
         List<Row> rows = groupedDataset.count().collectAsList();//JavaConversions.asScalaBuffer(words)).count();
         return rows.stream().map(new Function<Row, Count>() {
